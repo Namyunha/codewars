c287d8cc1ef4998af061bbf962fe862c88f8fecd
Namyunha <108773845+Namyunha@users.noreply.github.com>
2025-01-28 12:12:56 +0900

    train4 solution 1-1

---
+++ train/4/1-1.js
@@ -0,0 +1,28 @@
+// must return 2
+// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
+// must return 0
+// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+// cakes 함수 구현
+function cakes(recipe, available) {
+  return Math.min(
+    ...Object.keys(recipe).map((ingredient) => {
+      if (!available[ingredient]) return 0;
+
+      return Math.floor(available[ingredient] / recipe[ingredient]);
+    })
+  );
+}
+
+console.log(
+  cakes(
+    { flour: 500, sugar: 200, eggs: 1 },
+    { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+  )
+); // 2
+
+console.log(
+  cakes(
+    { apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 },
+    { sugar: 500, flour: 2000, milk: 2000 }
+  )
+); // 0
