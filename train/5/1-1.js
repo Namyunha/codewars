dd3577a8bcf41a9a3bd22fdd5254c386b0e318c5
Namyunha <108773845+Namyunha@users.noreply.github.com>
2025-01-29 16:55:06 +0900

    train5 solution 1-2

--- train/5/1-1.js
+++ train/5/1-1.js
@@ -1,8 +1,11 @@
-// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 function moveZeros(arr) {
-  return arr
-    .filter((item) => item !== 0)
-    .concat(arr.filter((item) => item === 0));
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === 0) {
+      arr.splice(i, 1);
+      arr.push(0);
+    }
+  }
+  return arr;
 }
 
-console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
+console.log(moveZeros([+0, 1, null, 2, false, 1, +0]));
